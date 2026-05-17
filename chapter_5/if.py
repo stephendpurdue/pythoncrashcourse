cdd53bb9be5d969ff09dcc06dd3325e99c709602
Stephen <stephendpurdue@gmail.com>
2026-05-17 15:42:29 +0100

    started chapter 5

---
+++ chapter_5/if.py
@@ -0,0 +1,9 @@
+cakes = ['Madeira', 'Chocolate', 'Victoria Sponge']
+
+for cake in cakes:
+    if cake == 'Chocolate':
+        print(cake.lower())
+    else:
+        print(cake.title())
+
+# Make sure to put brackets when using methods within if statements, else it will print an error.
