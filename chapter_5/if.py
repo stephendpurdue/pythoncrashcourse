4134bb7edfef8a699a5881dea1b8877e972ad2b6
Stephen <stephendpurdue@gmail.com>
2026-05-18 16:32:13 +0100

    # 5-1 & # 5-2

--- chapter_5/if.py
+++ chapter_5/if.py
@@ -6,4 +6,29 @@ for cake in cakes:
     else:
         print(cake.title())
 
+new_cake = 'Battenburg'
+
+if new_cake not in cakes:
+    print('Battenburg: This one is not very popular')
+
 # Make sure to put brackets when using methods within if statements, else it will print an error.
+
+requested_cake = 'Chocolate'
+
+if requested_cake != 'Madeira':
+    print('I do not like Madeira, Chocolate instead please!')
+
+# Numerical comparisons
+
+age = 10
+
+if age != 50:
+    print('You are too young!')
+
+# Checking multiple conditions
+
+age_1 = 18
+age_2 = 21
+
+if age_1 >= 21 and age_2 <= 21:
+    print('You are old enough to drink!')
---
+++ chapter_5/problem_set.py
@@ -0,0 +1,62 @@
+# 5-1 
+
+car = 'BMW'
+print("\nIs car == 'BMW'? I predict True.")
+print(car == 'BMW')
+
+car = 'Volkswagen'
+print("\nIs car == 'Volkswagen'? I predict True.")
+print(car == 'Volkswagen')
+
+car = 'Skoda'
+print("\nIs car == 'Volkswagen'? I predict False.")
+print(car == 'Volkswagen')
+
+car = 'BMW'
+print("\nIs car == 'Audi'? I predict False.")
+print(car == 'Audi')
+
+car = 'BMW'
+print("\nIs car == 'Skoda'? I predict False.")
+print(car == 'Skoda')
+
+car = 'Skoda'
+print("\nIs car == 'BMW'? I predict False.")
+print(car == 'BMW')
+
+car = 'BMW'
+print("\nIs car == 'BMW'? I predict True.")
+print(car == 'BMW')
+
+car = 'Skoda'
+print("\nIs car == 'Volkswagen'? I predict False.")
+print(car == 'Volkswagen')
+
+car = 'Toyota'
+print("\nIs car == 'Toyota'? I predict True.")
+print(car == 'Toyota')
+
+car = 'Toyota'
+print("\nIs car == 'Volkswagen'? I predict False.")
+print(car == 'Volkswagen')
+
+# 5-2
+
+car = 'BMW'
+print("\nIs car == 'Volkswagen'? I predict False.")
+print(car.lower() == 'Volkswagen')
+
+amount_of_people = 5
+
+if amount_of_people >= 4:
+    print('Busy!')
+
+if amount_of_people != 6:
+    print('Not that busy!')
+
+cake = ['Chocolate', 'Battenburg']
+other_cakes = 'Madeira', 'Victoria Sponge'
+
+if other_cakes not in cake:
+    print('It has to be chocolate!')
+    
