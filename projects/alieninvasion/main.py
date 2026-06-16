8df1e1c9eabbfaa40b34c315a8ae857fd239bd17
Stephen <stephendpurdue@gmail.com>
2026-06-16 08:39:45 +0100

    created game_functions.py

--- projects/alieninvasion/main.py
+++ projects/alieninvasion/main.py
@@ -2,11 +2,12 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     # Initializes the game, creates a display window
     pygame.init()
-    # Creates an instance called ai_settings, which pulls from the Settings module.
+    # Creates an instance called ai_settings, which pulls from the Settings module
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
@@ -18,14 +19,9 @@ def run_game():
     # Starts the main game loop
     while True:
 
-        # Listens for keyboard and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        pygame.display.flip()
+        # All of these functions are self contained in their own modules
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 # Runs the game
 run_game()
--- projects/alieninvasion/notes.md
+++ projects/alieninvasion/notes.md
@@ -2,3 +2,5 @@
 
  - main.py has the core gameplay logic, and will be added to over time
  - settings.py has the main gameplay settings and will be changed over time as new features are added to main.py
+ - ship.py draws the ship to the screen
+ - game_functions.py controls and detects user inputs and other game functions
