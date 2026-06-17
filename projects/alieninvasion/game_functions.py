f1485d82419b50c4dc63e79ba8100a1fea86fc20
Stephen <stephendpurdue@gmail.com>
2026-06-17 15:48:23 +0100

    refactor: refactored ship controls to two separate functions

--- projects/alieninvasion/game_functions.py
+++ projects/alieninvasion/game_functions.py
@@ -6,20 +6,22 @@ def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        
-        # Ship controls
         elif event.type == pygame.KEYDOWN:
+             check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+             check_keyup_events(event, ship)
+
+        # Ship controls
+        def check_keydown_events(event, ship):
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT:
                     ship.moving_left = True
-            
-        elif event.type == pygame.KEYUP:
+
+        def check_keyup_events(event, ship):   
             if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT:
                     ship.moving_left = False
         
         
