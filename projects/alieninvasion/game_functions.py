6d83b33de7fbc77d5dad9cb134913f71d8c422e2
Stephen <stephendpurdue@gmail.com>
2026-07-01 16:12:14 +0100

    feature: completed button

--- projects/alieninvasion/game_functions.py
+++ projects/alieninvasion/game_functions.py
@@ -138,22 +138,33 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 
 # Events and Screen
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
     """Respond to keyboard and mouse inputs"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_play_button(stats, play_button, mouse_x, mouse_y)
         
         elif event.type == pygame.KEYDOWN:
              check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
              check_keyup_events(event, ship)
 
-def update_screen(ai_settings, screen, ship, aliens, bullets):
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+    """Start new game on play"""
+    if play_button.rect.collidepoint(mouse_x, mouse_y):
+        stats.game_active = True
+
+def update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button):
     """Update images on the screen and flip to the new screen"""
     screen.fill(ai_settings.bg_color)
     ship.blitme()
     aliens.draw(screen)
+
+    if not stats.game_active:
+        play_button.draw_button()
 
     for bullet in bullets.sprites():
         bullet.draw_bullet()
--- projects/alieninvasion/main.py
+++ projects/alieninvasion/main.py
@@ -6,6 +6,7 @@ from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
 from game_stats import GameStats
+from button import Button
 
 def run_game():
     # Initializes the game, creates a display window
@@ -20,20 +21,21 @@ def run_game():
     bullets = Group()
     aliens = Group()
     pygame.display.set_caption("Alien Invaders")
+    play_button = Button(ai_settings, screen, "Play")
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # Starts the main game loop
     while True:
 
         # All of these functions are self contained in their own modules
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
 
         if stats.game_active: 
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
             
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 # Runs the game
 run_game()
