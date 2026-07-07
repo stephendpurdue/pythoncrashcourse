65a59d21fb8171aa0020eb982d1c84db7af08e2c
Stephen <stephendpurdue@gmail.com>
2026-07-07 08:09:35 +0100

    finished matplotlib

--- projects/datavisualisation/rw_visual.py
+++ projects/datavisualisation/rw_visual.py
@@ -1,9 +1,16 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=12)
-plt.show()
+while True:
+    rw = RandomWalk(50000)
+    rw.fill_walk()
+    
+    point_numbers = list(range(rw.num_points))
+    plt.plot(rw.x_values, rw.y_values, c=point_numbers, edgecolors='none', s=1)
+    plt.show()
+
+    keep_running = input("Run again? (y/n):")
+    if keep_running == 'n':
+        break
 
 plt.savefig('randomwalkimages/random_walk.png', bbox_inches='tight')
