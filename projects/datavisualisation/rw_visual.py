91a1b493afa056f856aa2b2ed24c4b568d0212fe
Stephen <stephendpurdue@gmail.com>
2026-07-08 08:22:08 +0100

    feature: added view and add to todolist

--- projects/datavisualisation/rw_visual.py
+++ projects/datavisualisation/rw_visual.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
+run_number = 1
+
 while True:
-    rw = RandomWalk(50000)
+    rw = RandomWalk(5000)
     rw.fill_walk()
     
     point_numbers = list(range(rw.num_points))
-    plt.plot(rw.x_values, rw.y_values, c=point_numbers, edgecolors='none', s=1)
-    plt.show()
+    plt.figure()
+    plt.plot(rw.x_values, rw.y_values, c=point_numbers, lw=1, edgecolors='none', s=1)
+    
+    filename = f"randomwalkimages/random_walk_{run_number}.png"
+    plt.savefig('randomwalkimages/random_walk.png', bbox_inches='tight')
 
     keep_running = input("Run again? (y/n):")
     if keep_running == 'n':
         break
 
-plt.savefig('randomwalkimages/random_walk.png', bbox_inches='tight')
+    run_number += 1 # Increments the run count after each successful run.
